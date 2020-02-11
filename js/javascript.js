02e4b63dc669b4e90938588d1f3520be6080c93c
GraceKwon0507 <54426922+GraceKwon0507@users.noreply.github.com>
2020-02-10 18:03:39 -0600

    give form an id in order to use the reset button

--- js/javascript.js
+++ js/javascript.js
@@ -3,8 +3,16 @@ function init() {
 
     function showMeText() {
         alert("Thank you for submitting your information!");
+        document.getElementById("myForm").reset();
     }
-
     button.addEventListener('click', showMeText);
+
+    var button = document.getElementById('resetButton');
+
+    function reset() {
+        alert("Are you sure you want to reset all the information?");
+        document.getElementById("myForm").reset();
+    }
+    button.addEventListener('click', reset);
 }
 window.addEventListener('load', init);
