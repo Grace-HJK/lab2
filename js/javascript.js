05340368efc1926268a1a37a845e07bec4c418ab
GraceKwon0507 <54426922+GraceKwon0507@users.noreply.github.com>
2020-02-10 18:45:02 -0600

    adding javascript to check if any input is a null value

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,20 @@ function init() {
     var button = document.getElementById('submitButton');
 
     function showMeText() {
-        alert("Thank you for submitting your information!");
-        document.getElementById("myForm").reset();
+        var phone = document.forms["myForm"]["phone_field"].value;
+        var email = document.forms["myForm"]["email_field"].value;
+        var bday = document.forms["myForm"]["date_of_birth"].value;
+        if (phone === "" || email === "" || bday === "")
+        {
+            alert("Please fill in the form");
+            return false;
+        }
+        else
+        {
+            alert("Thank you for submitting your information!");
+            document.getElementById("myForm").reset();
+            return true;
+        }
     }
     button.addEventListener('click', showMeText);
 
